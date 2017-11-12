0b594f3a63d79af4501d84847e26abc68524d31e
SalahEddineBOURAGBA <33601625+SalahEddineBOURAGBA@users.noreply.github.com>
2017-11-12 22:23:12 +0100

    update crc

--- functions.h
+++ functions.h
@@ -6,6 +6,9 @@
 
 std::vector <unsigned int> crc(std::vector <unsigned int> const& input,const unsigned int crc_version);
 unsigned int xor_with_1(const unsigned int a);
-void print_table(const std::vector <unsigned int>& table);
+std::vector <unsigned int> stuff_bit_count(std::vector <unsigned int> const& input);
+
+void print_table(std::vector <unsigned int> const& table);
+void print_table_inverted(std::vector <unsigned int> const& table);
 
 #endif // FONCTIONS_H_INCLUDED
