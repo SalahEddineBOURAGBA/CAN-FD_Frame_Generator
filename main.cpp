b2a859538f16c9b4833e02512f6865af4b8b8719
SalahEddineBOURAGBA <33601625+SalahEddineBOURAGBA@users.noreply.github.com>
2017-11-12 19:02:45 +0100

    CRC Calculation
    Added a CRC calculation function

--- main.cpp
+++ main.cpp
@@ -4,18 +4,14 @@ using namespace std;
 
 int main()
 {
-    vector <unsigned int> zeros (15,0);
     vector <unsigned int> frame {0,0,0,0,0,0,1,0,0,1,0,1,0,1,0,0,0,1,1,0,1,1,0,0,1,0,1,1,1};
-    vector <unsigned int> input_crc;
     vector <unsigned int> output_crc(15);
 
-    //Union of two tables
-    input_crc.insert(input_crc.begin(),frame.begin(),frame.end());
-    input_crc.insert(input_crc.begin(),zeros.begin(),zeros.end());
-    print_table(input_crc);
-
     //calculate crc_15
-    output_crc=crc_15(input_crc);
+    output_crc=crc_15(frame);
+
+    print_table(frame);
+    print_table(output_crc);
 
     return 0;
 }
