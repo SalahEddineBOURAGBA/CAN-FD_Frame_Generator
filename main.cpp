c96bf8ca0cfa1269e03e0e147c5bf7561961b31f
SalahEddineBOURAGBA <33601625+SalahEddineBOURAGBA@users.noreply.github.com>
2017-11-13 13:08:34 +0100

    Analyse frame
    Added Analyse Frame.
    No CAN frames yet.
    No stuff bits yet.

--- main.cpp
+++ main.cpp
@@ -4,14 +4,27 @@ using namespace std;
 
 int main()
 {
-    vector <unsigned int> frame {0,0,0,0,0,0,1,0,0,1,0,1,0,1,0,0,0,1,1,0,1,1,0,0,1,0,1,1,1};
-    vector <unsigned int> output_crc(15);
+    //No stuff bits yet
+    //Only CAN FD frames, no CAN frames
 
-    //calculate crc_15
-    output_crc=crc_15(frame);
+    //Tampon frame from SOF to last Data bit
+    vector <unsigned int> frame {0,0,0,0,1,1,0,0,1,1,1,0,0,0,1,0,0,1,1,0,0,0,1,0,0,1,1,1,0,1};
+    //Tampon frame from CRC delimiter to last interframe bit
+    vector <unsigned int> frame2 {1,0,1,1,1,1,1,1,1,1,1,1,1};
+    //stuff count and CRC
+    vector <unsigned int> crc_output,stuff_count;
 
-    print_table(frame);
-    print_table(output_crc);
+    //Calculate stuff count and CRC
+    stuff_count=stuff_bit_count(frame);
+    crc_output=crc(frame,17);
+
+    //Complete frame
+    frame.insert(frame.end(),stuff_count.begin(),stuff_count.end());
+    frame.insert(frame.end(),crc_output.begin(),crc_output.end());
+    frame.insert(frame.end(),frame2.begin(),frame.end());
+
+    //Analyse frame
+    analyse_frame(frame);
 
     return 0;
 }
