125652c432a2152d0d4480a7b371acee5aa86eca
SalahEddineBOURAGBA <33601625+SalahEddineBOURAGBA@users.noreply.github.com>
2017-12-22 00:26:48 +0100

    Final version

--- main.cpp
+++ main.cpp
@@ -1,32 +1,25 @@
+#include <vector>
+#include "analyzer_functions.h"
+#include "generator_functions.h"
 #include "functions.h"
+#include "test.h"
 
 using namespace std;
 
 int main()
 {
-    //No stuff bits yet
-    //Only CAN FD frames, no CAN frames
+    vector <bool> frame;
 
-    //Tampon frame from SOF to last Data bit
-    vector <unsigned int> frame {0, 0,0,0,0,1,0,0,1,1,1,1,1,0,0, 0,1,0,1,1,1,0,0,0, 0,0,1,0,1,1,1,1,1};
-    //Tampon frame from 1st fixed stuff bit to last CRC bit
-    vector <unsigned int> frame2 {0,0,1,0,1,0,1,1,1,1,0,0,1,1,0,1,0,1,1,1,0,1,1,1,1,0,0};
-    //Tampon frame from CRC delimiter to last interframe bit
-    vector <unsigned int> frame3 {1,0,1,1,1,1,1,1,1,1,1,1,1};
-    //Complete frame
-    frame.insert(frame.end(),frame2.begin(),frame2.end());
-    frame.insert(frame.end(),frame3.begin(),frame3.end());
+    //Generate 10 random frames and analyze them
+    for(unsigned int i=0;i<10;i++)
+    {
+        analyze_frame(generate_frame());
+        cout<<"------------------------------------------------"<<endl;
+    }
 
-
-    //Analyse frame
-    analyse_frame(frame);
-
-    /*
-    vector <unsigned int> crc_output,stuff_count;
-    stuff_count=stuff_bit_count(3);
-    crc_output=crc(frame,17);
-    print_table(stuff_count);
-    print_table(crc_output);*/
+    //Test the generator function
+    //test_generator(1000);
 
     return 0;
 }
+
