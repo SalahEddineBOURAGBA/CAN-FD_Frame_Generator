a57d6c5e100706fde89ccf38871c9a775efc8918
SalahEddineBOURAGBA <33601625+SalahEddineBOURAGBA@users.noreply.github.com>
2017-12-21 17:39:23 +0100

    change functions.h
    Return data_length from DLC field.
    Print and Compare Tables.

--- functions.h
+++ functions.h
@@ -1,32 +1,20 @@
-#ifndef FONCTIONS_H_INCLUDED
-#define FONCTIONS_H_INCLUDED
+#ifndef FONCTIONS
+#define FONCTIONS
 
-#include "iostream"
-#include "vector"
-#include <algorithm>
+#include <iostream>
+#include <vector>
 
-/*------------------------------------------------------------------------------
-------------------------------Analyze a given frame-----------------------------
-------------------------------------------------------------------------------*/
-void analyse_frame(std::vector <unsigned int> const& frame);
-unsigned int data_length_from_dlc(std::vector <unsigned int> CRC);
-void display_frame(std::vector <unsigned int> const& identifier, std::vector <unsigned int> const& ,
-                   std::vector <unsigned int> const& stuff_count, std::vector <unsigned int> const& CRC,
-                   unsigned int IDE, unsigned int BRS, unsigned int ESI, unsigned int data_length);
-
-/*------------------------------------------------------------------------------
---------------------------------CRC Calculation---------------------------------
-------------------------------------------------------------------------------*/
-std::vector <unsigned int> crc(std::vector <unsigned int> const& input,const unsigned int crc_version);
-unsigned int xor_with_1(const unsigned int a);
-std::vector <unsigned int> stuff_bit_count(unsigned int stuff);
+unsigned int data_length_from_dlc(std::vector <bool> const& DLC);
 
 /*-------------------------------------------------------------------------------
 ----------------------------------Table manipulation-----------------------------
 -------------------------------------------------------------------------------*/
-void print_table(std::vector <unsigned int> const& table);
-void print_table_inverted(std::vector <unsigned int> const& table);
-void print_stuff_count(std::vector <unsigned int> const& table);
-unsigned int compare_two_tables(std::vector <unsigned int> const& table1,std::vector <unsigned int> const& table2);
+
+void print_table(std::vector <bool> const& table);
+void print_table_inverted(std::vector <bool> const& table);
+void print_stuff_count(std::vector <bool> const& table);
+bool compare_two_tables(std::vector <bool> const& table1,std::vector <bool> const& table2);
+
+
 
 #endif // FONCTIONS_H_INCLUDED
