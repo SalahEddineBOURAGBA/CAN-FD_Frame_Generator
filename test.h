be4b164715cc80803b84ed35bc4d13d8b0c78fb4
SalahEddineBOURAGBA <33601625+SalahEddineBOURAGBA@users.noreply.github.com>
2017-12-22 00:23:19 +0100

    Working version
    test the generator function

---
+++ test.h
@@ -0,0 +1,12 @@
+#ifndef TEST
+#define TEST
+
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <string>
+
+void test_generator(unsigned int MaxSize);
+std::vector <bool> convert_string_to_bool_vector(std::string input);
+
+#endif // TEST
