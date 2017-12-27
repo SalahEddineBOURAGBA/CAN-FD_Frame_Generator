53c73eb9fd358fc9e6e90452d2617cd57151af91
SalahEddineBOURAGBA <33601625+SalahEddineBOURAGBA@users.noreply.github.com>
2017-12-27 10:50:57 +0100

    update display
    bit by bit display
    move functions to functions.h

--- generator_functions.h
+++ generator_functions.h
@@ -6,17 +6,11 @@
 #include <sys/time.h>
 #include <vector>
 #include "functions.h"
-#include "analyzer_functions.h"
 
-//The main function to generate a random frame
+/*------------------------------------------------------------------------------
+------------------------------Generate a frame----------------------------------
+------------------------------------------------------------------------------*/
 std::vector <bool> generate_frame();
-
-//Stuff the frame
-unsigned int stuff_frame(std::vector <bool>& frame, bool can);
-
-//Fixed Stuffed CRC Field
-std::vector <bool> stuff_crc(std::vector <bool> const& stuff_count, std::vector <bool> const& crc,
-                             const bool first_bit);
 
 
 #endif // GENERATOR_FUNCTIONS
