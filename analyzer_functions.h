a0143c152fdc013e7d2d4d7d642fab08aeca2725
SalahEddineBOURAGBA <33601625+SalahEddineBOURAGBA@users.noreply.github.com>
2017-12-27 10:45:46 +0100

    Update display
    display all bits
    move other functions to functions.h

--- analyzer_functions.h
+++ analyzer_functions.h
@@ -2,27 +2,14 @@
 #define ANALYZER_FUNCTIONS
 
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include "functions.h"
 
 /*------------------------------------------------------------------------------
 ------------------------------Analyze a given frame-----------------------------
 ------------------------------------------------------------------------------*/
-
 bool analyze_frame(std::vector <bool> const& frame);
-void display_analyzed_frame(std::vector <bool> const& identifier, std::vector <bool> const&  DLC,
-                            std::vector <bool> const&  DATA, std::vector <bool> const& stuff_count,
-                            std::vector <bool> const& CRC, const bool IDE, const bool FDF, const bool BRS, const bool ESI);
-
-
-
-/*------------------------------------------------------------------------------
---------------------------------CRC Calculation---------------------------------
-------------------------------------------------------------------------------*/
-
-std::vector <bool> crc(std::vector <bool> const& input,const unsigned int crc_version);
-std::vector <bool> stuff_bit_count(const unsigned int stuff);
-std::vector <bool> unstuff_frame(std::vector <bool> const& input);
 
 
 #endif // FONCTIONS_H_INCLUDED
