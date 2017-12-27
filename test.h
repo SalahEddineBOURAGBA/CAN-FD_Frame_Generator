596e17d0e26285a7fdb541d42e6c57df99513cc1
SalahEddineBOURAGBA <33601625+SalahEddineBOURAGBA@users.noreply.github.com>
2017-12-27 10:51:58 +0100

    Update display

--- test.h
+++ test.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include "generator_functions.h"
+#include "analyzer_functions.h"
 
 void test_generator(unsigned int MaxSize);
 std::vector <bool> convert_string_to_bool_vector(std::string input);
