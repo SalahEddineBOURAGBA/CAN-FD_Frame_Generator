4e132628af1cb46b45e889b8bc0b712d60dc240c
SalahEddineBOURAGBA <33601625+SalahEddineBOURAGBA@users.noreply.github.com>
2018-07-11 14:26:28 +0200

    independent from std
    replaced std:vector with embeddedvector library

--- generator_functions.h
+++ generator_functions.h
@@ -3,14 +3,35 @@
 
 
 #include <stdlib.h>
-#include <sys/time.h>
-#include <vector>
-#include "functions.h"
+#include "embeddedvector.h"
+#include "CANFDFrame.h"
+
+
+/*------------------------------------------------------------------------------
+--------------------------------CRC Calculation---------------------------------
+------------------------------------------------------------------------------*/
+//Return the CRC of a frame
+embeddedvector <bool> crc(const embeddedvector <bool>& input,const unsigned int crc_version);
+//return the stuff bit count
+embeddedvector <bool> stuff_bit_count(const unsigned int stuff);
+
+
+/*------------------------------------------------------------------------------
+--------------------------------Stuffing----------------------------------------
+------------------------------------------------------------------------------*/
+//Stuff a CAN or CAN FD frame
+unsigned int stuff_frame(embeddedvector <bool>& frame, bool can);
+//Fixed Stuffed CRC Field for CAN FD
+embeddedvector <bool> stuff_crc(embeddedvector <bool> const& stuff_count, embeddedvector <bool> const& crc,
+                             const bool first_bit);
+//Unstuff a CAN or CAN FD frame
+embeddedvector <bool> unstuff_frame(embeddedvector <bool> const& input);
+
 
 /*------------------------------------------------------------------------------
 ------------------------------Generate a frame----------------------------------
 ------------------------------------------------------------------------------*/
-std::vector <bool> generate_frame();
+embeddedvector <bool> generate_frame(const CANFDFrame &inFrame);
 
 
 #endif // GENERATOR_FUNCTIONS
